24df48b7ecfcdbc6a12c5b33cd187e25b7b1a35d
AminaSultana <lilisultana53@gmail.com>
2022-04-11 19:39:40 +0530

    expenseItem

---
+++ src/Component/ExpenseItem.js
@@ -0,0 +1,16 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div className="expense-item__description">
+        <ExpenseDate date={props.date}/>
+        <ExpenseDetails title={props.title} amount={props.amount} location={props.location}/>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
