cc8e7cc4860fc9e027b747c5ee52e2fa18326efe
AminaSultana <lilisultana53@gmail.com>
2022-04-14 15:25:01 +0530

    task 8

---
+++ src/Component/Expenses/ExpenseFilter.js
@@ -0,0 +1,24 @@
+import "./ExpenseFilter.css";
+
+const ExpenseFilter = (props) => {
+  const dropdownChangeHandler = (e) => {
+    props.onChangeFilter(e.target.value);
+  };
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <h2>Filter by Year</h2>
+        <label>Select year</label>
+        <select value={props.selectedYear} onChange={dropdownChangeHandler}>
+          <option value="2019">2019</option>
+          <option value="2020">2020</option>
+          <option value="2021">2021</option>
+          <option value="2022">2022</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseFilter;
